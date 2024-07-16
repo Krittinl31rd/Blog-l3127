61d4145a0e2665dfa121577bcdb9fbaef3188deb
Hummingl31rd <88831732+Krittinl31rd@users.noreply.github.com>
2024-07-16 22:25:33 +0700

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // 30 seconds
+      socketTimeoutMS: 45000, // 45 seconds
+    });
     console.log("mongoose connected");
   } catch (err) {
     console.log(err);
