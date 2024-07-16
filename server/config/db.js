5925acb16ebdb0324063e32f9e32784901a0a28b
Hummingl31rd <88831732+Krittinl31rd@users.noreply.github.com>
2024-07-16 22:15:45 +0700

    lasted

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017");
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("mongoose connected");
   } catch (err) {
     console.log(err);
